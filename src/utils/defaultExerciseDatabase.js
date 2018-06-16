71bb57407d3a217f1447e413bd96c89bdaee78c4
Zanin Andrea <99.zanin@gmail.com>
2018-06-16 10:02:03 +0200

    added some exercises

--- src/utils/defaultExerciseDatabase.js
+++ src/utils/defaultExerciseDatabase.js
@@ -1,6 +1,21 @@
 export default [
   {
+    name: "Muscle Up",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Muscle Up (kipping)",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
     name: "Pull Up",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Pull Up (explosive)",
     showActiveTime: false,
     showWeight: false
   },
@@ -25,6 +40,11 @@ export default [
     showWeight: false
   },
   {
+    name: "Front Lever (straddle)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
     name: "Back Lever (tucked)",
     showActiveTime: true,
     showWeight: false
@@ -36,6 +56,11 @@ export default [
   },
   {
     name: "Back Lever",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Back Lever (straddle)",
     showActiveTime: true,
     showWeight: false
   },
@@ -70,6 +95,21 @@ export default [
     showWeight: false
   },
   {
+    name: "Push Up (sphinx)",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Push Up (sphinx weighted)",
+    showActiveTime: false,
+    showWeight: true
+  },
+  {
+    name: "Reverse Push Up",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
     name: "Push Up (diamond weighted)",
     showActiveTime: false,
     showWeight: true
@@ -82,6 +122,136 @@ export default [
   {
     name: "Dips (bar)",
     showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Hang Leg Raise",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Crunch",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Sit Up",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Hollow Crunch",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Dragonflag",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Dragonflag (negative)",
+    showActiveTime: false,
+    showWeight: false
+  },
+  {
+    name: "Dragonflag (hold)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Hollow body hold",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Handstand",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Handstand (wall)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Frogstand",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Frogstand (one-knee)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Planche",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Planche (bent arm)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Planche (bent arm straddle)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Wall sit",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Wall sit (one-leg)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "Wall sit",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "L sit (floor)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "L sit (rings)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "L sit (hanging)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "L sit (parallel bars)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "V sit (floor)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "V sit (rings)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "V sit (hanging)",
+    showActiveTime: true,
+    showWeight: false
+  },
+  {
+    name: "V sit (parallel bars)",
+    showActiveTime: true,
     showWeight: false
   }
 ];
